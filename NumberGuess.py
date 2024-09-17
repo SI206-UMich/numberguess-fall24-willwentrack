75d73dbf21273d4ff15ba5456aa4409c036559f4
Will <wwentrac@umich.edu>
2024-09-17 11:47:48 -0400

    Test

--- NumberGuess.py
+++ NumberGuess.py
@@ -1,6 +1,6 @@
 import random
 class NumberGuess:
-    # TESTING TESTING TESTING
+    # TESTING TESTING TESTING PLEASE WORK
     def __init__(self):
         self.number = random.randint(1, 100)
         self.guess = -1
